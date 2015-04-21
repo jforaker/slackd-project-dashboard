9ede26da96df1faac9e124e4af76107db39476fa
Jake Foraker <jake.foraker@byjakt.com>
2015-04-21 10:52:18 -0400

    foo

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,10 +1,6 @@
 module.exports = {
     postgres: {
         connector: 'postgresql',
-        // hostname: process.env.DB_HOST || 'localhost',
-        // port: process.env.DB_PORT || 27017,
-        //user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
         url: process.env.DATABASE_URL
     }
 };
